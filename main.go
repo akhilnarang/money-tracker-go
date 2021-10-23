c07bf699c08152742a3bbfab695ac4357a4643c6
Akhil Narang <me@akhilnarang.dev>
2021-10-23 12:29:38 +0530

    Add some comments
    Signed-off-by: Akhil Narang <me@akhilnarang.dev>

--- main.go
+++ main.go
@@ -18,12 +18,15 @@ var port = flag.Int("port", 8000, "Port to listen on")
 func InitDb() {
 	var err error
 
+	// Fetch our DB URI from the environment
 	dsn := os.Getenv("DATABASE_URI")
 
+	// Ensure its set
 	if dsn == "" {
 		panic("Please provide a proper database URI")
 	}
 
+	// Open a connection to the database
 	database.Db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic(err.Error())
@@ -31,16 +34,22 @@ func InitDb() {
 }
 
 func main() {
+	// Parse flags
 	flag.Parse()
 
+	// Create our fiber app
 	app := fiber.New(fiber.Config{AppName: "Money Tracker", Prefork: true})
 
+	// Use recover middleware
 	app.Use(recover.New())
 
+	// Initialize DB connection
 	InitDb()
 
+	// Start declaring our routes
 	app.Get("/", handlers.Root)
 
+	// Create a group for /api/v1 prefix
 	apiV1 := app.Group("/api/v1")
 
 	apiV1.Get("/", handlers.GetExpenses)
@@ -51,6 +60,6 @@ func main() {
 
 	apiV1.Delete("/:id", handlers.DeleteExpense)
 
+	// Start our fiber app
 	log.Fatal(app.Listen(fmt.Sprintf(":%d", *port)))
-
 }
